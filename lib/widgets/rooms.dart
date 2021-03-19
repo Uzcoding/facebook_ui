137f9fde3023c86d21ce621bcb965d860156d26c
uzcoding <millioner.sm.8@gmail.com>
2021-03-19 23:53:11 +0500

    home_screen finished

---
+++ lib/widgets/rooms.dart
@@ -0,0 +1,91 @@
+import 'package:facebook_ui/config/palette.dart';
+import 'package:facebook_ui/models/models.dart';
+import 'package:facebook_ui/widgets/widgets.dart';
+import 'package:flutter/material.dart';
+
+class Rooms extends StatelessWidget {
+  final List<User> onlineUsers;
+
+  const Rooms({
+    Key key,
+    @required this.onlineUsers,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final bool isDesktop = Responsive.isDesktop(context);
+    return Card(
+      margin: EdgeInsets.symmetric(horizontal: isDesktop ? 5.0 : 0.0),
+      elevation: isDesktop ? 1.0 : 0.0,
+      child: Container(
+        height: 60.0,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: isDesktop ? BorderRadius.circular(10.0) : null,
+        ),
+        child: ListView.builder(
+          padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 4.0),
+          scrollDirection: Axis.horizontal,
+          itemCount: 1 + onlineUsers.length,
+          itemBuilder: (BuildContext context, int index) {
+            if (index == 0) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: _CreateRoomButton(),
+              );
+            }
+            final User user = onlineUsers[index - 1];
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: ProfileAvatar(
+                imageUrl: user.imageUrl,
+                isActive: true,
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class _CreateRoomButton extends StatelessWidget {
+  const _CreateRoomButton({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return OutlinedButton(
+      onPressed: () => print('Create Room'),
+      style: OutlinedButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        side: BorderSide(
+          width: 3,
+          color: Colors.blueAccent[100],
+        ),
+        textStyle: TextStyle(color: Palette.facebookBlue),
+      ),
+      child: Row(
+        children: [
+          // ShaderMask(
+          //   shaderCallback: (rect) =>
+          //       Palette.createRoomGradient.createShader(rect),
+          //   child: Icon(
+          //     Icons.video_call,
+          //     size: 35.0,
+          //     color: Colors.white,
+          //   ),
+          // ),
+          Icon(
+            Icons.video_call,
+            size: 35.0,
+            color: Colors.purple,
+          ),
+          const SizedBox(width: 4.0),
+          Text('Create\nRoom'),
+        ],
+      ),
+    );
+  }
+}
